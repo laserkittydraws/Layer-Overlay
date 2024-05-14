e1c22bc9b682ee46fefd919cec012dafb08d06a2
laserkittydraws <laserkittydraws@gmail.com>
2024-05-14 11:45:45 -0400

    removed unnecessary class variables

--- src/MLEM/mlem.py
+++ src/MLEM/mlem.py
@@ -9,9 +9,6 @@ EXTENSION_VERSION = '0.2.0'
 
 class MiniOverlayExaminerModuleExtension(Extension):
     
-    kritaInst: Krita = Krita.instance()
-    notifier: Notifier = kritaInst.notifier()
-    activeWindow: Window
     activeWindowUpdated: bool = False
     
     view: View
@@ -26,10 +23,12 @@ class MiniOverlayExaminerModuleExtension(Extension):
 
     def setup(self):
         
+        notifier: Notifier = Krita.instance().notifier()
+        
         # setup signals
-        self.notifier.viewCreated.connect(self.updateViews)
-        self.notifier.viewClosed.connect(self.updateViews)
-        self.notifier.windowCreated.connect(self.updateActiveWindow)
+        notifier.viewCreated.connect(self.updateViews)
+        notifier.viewClosed.connect(self.updateViews)
+        notifier.windowCreated.connect(self.updateActiveWindow)
     
     def createActions(self, window: Window) -> None:
         axn = window.createAction('toggleLayerOverlay', 'Toggle Layer Overlay', 'Tools/Scripts')
@@ -37,67 +36,66 @@ class MiniOverlayExaminerModuleExtension(Extension):
         
     def updateActiveWindow(self) -> None:
         if not self.activeWindowUpdated:
-            self.activeWindow: Window = self.kritaInst.activeWindow()
-            self.activeWindow.activeViewChanged.connect(self.updateView)
+            Krita.instance().activeWindow().activeViewChanged.connect(self.updateView)
             
             # navigating layers
-            self.kritaInst.action('activateNextLayer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('activateNextSiblingLayer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('activatePreviousLayer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('activatePreviousSiblingLayer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('activateNextLayer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('activateNextSiblingLayer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('activatePreviousLayer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('activatePreviousSiblingLayer').triggered.connect(self.updateLayerOverlayLayers)
             
             # misc
-            self.kritaInst.action('merge_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('duplicatelayer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('flatten_image').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('remove_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('RenameCurrentLayer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('paste_layer_from_clipboard').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('new_from_visible').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('move_layer_down').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('move_layer_up').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('cut_layer_clipboard').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('copy_selection_to_new_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('cut_selection_to_new_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('split_alpha_into_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('merge_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('duplicatelayer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('flatten_image').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('remove_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('RenameCurrentLayer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('paste_layer_from_clipboard').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('new_from_visible').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('move_layer_down').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('move_layer_up').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('cut_layer_clipboard').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('copy_selection_to_new_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('cut_selection_to_new_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('split_alpha_into_mask').triggered.connect(self.updateLayerOverlayLayers)
             
             # convert layer to...
-            self.kritaInst.action('convert_to_filter_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('convert_to_paint_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('convert_to_selection_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('convert_to_transparency_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('convert_to_file_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('convert_to_filter_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('convert_to_paint_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('convert_to_selection_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('convert_to_transparency_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('convert_to_file_layer').triggered.connect(self.updateLayerOverlayLayers)
             
             # import layer as...
-            self.kritaInst.action('import_layer_as_filter_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('import_layer_as_paint_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('import_layer_as_selection_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('import_layer_as_transparency_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('import_layer_from_file').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('import_layer_as_filter_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('import_layer_as_paint_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('import_layer_as_selection_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('import_layer_as_transparency_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('import_layer_from_file').triggered.connect(self.updateLayerOverlayLayers)
             
             # create new layer actions
-            self.kritaInst.action('add_new_clone_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_colorize_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_file_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_fill_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_adjustment_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_filter_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_group_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_selection_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_paint_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_transform_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_transparency_mask').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('add_new_shape_layer').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('new_from_visible').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_clone_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_colorize_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_file_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_fill_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_adjustment_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_filter_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_group_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_selection_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_paint_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_transform_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_transparency_mask').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('add_new_shape_layer').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('new_from_visible').triggered.connect(self.updateLayerOverlayLayers)
             
             # grouping of layers
-            self.kritaInst.action('create_quick_group').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('quick_ungroup').triggered.connect(self.updateLayerOverlayLayers)
-            self.kritaInst.action('create_quick_clipping_group').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('create_quick_group').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('quick_ungroup').triggered.connect(self.updateLayerOverlayLayers)
+            Krita.instance().action('create_quick_clipping_group').triggered.connect(self.updateLayerOverlayLayers)
             
             
             # canvas only mode switching
-            self.kritaInst.action('view_show_canvas_only').triggered.connect(self.updateLayerOverlayPosition)
+            Krita.instance().action('view_show_canvas_only').triggered.connect(self.updateLayerOverlayPosition)
     
     # MARK: UI FUNCTIONS
     
